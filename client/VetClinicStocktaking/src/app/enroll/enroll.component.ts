bfcb9a60e1645e37b960533e3773ab2841a8e034
Seva Ostapchuk <jawbreaker228@gmail.com>
2018-05-17 23:21:48 +0300

    progress

--- client/VetClinicStocktaking/src/app/enroll/enroll.component.ts
+++ client/VetClinicStocktaking/src/app/enroll/enroll.component.ts
@@ -10,6 +10,8 @@ export class EnrollComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  forthFormGroup: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) { }
 
@@ -19,6 +21,12 @@ export class EnrollComponent implements OnInit {
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required]
+    });
+    this.forthFormGroup = this._formBuilder.group({
+      forthCtrl: ['', Validators.required]
     });
   }
 
